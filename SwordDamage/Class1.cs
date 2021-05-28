f82bdc9bac105d016b27ce21d521a0d0496aced5
Meecosha <meecosha@gmail.com>
2021-05-28 15:37:06 +0300

    Add project files.

---
+++ SwordDamage/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwordDamage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwordDamage damage = new SwordDamage();
+            
+            while (true)
+            {
+                Console.WriteLine("Press 1 if only base damage\nPress 2 if only Magic\nPress 3 if only Flame\nPress 4 if both Magic and Flame\nPress anything else to quit");
+                char PressedKey = Console.ReadKey().KeyChar;
+                if (PressedKey == '1')
+                {
+                    damage.CalculateDamage();
+                }
+                else if (PressedKey == '2')
+                {
+                    damage.isMagic = true;
+                    damage.CalculateDamage();
+                }
+                else if (PressedKey == '3')
+                {
+                    damage.isFlame = true;
+                    damage.CalculateDamage();
+                }
+                else if (PressedKey == '4')
+                {
+                    damage.isMagic = true;
+                    damage.isFlame = true;
+                    damage.CalculateDamage();
+                }
+                else
+                {
+                    return;
+                }
+                
+                
+            }
+                       
+        }
+    }
+}
---
+++ SwordDamage/SwordDamage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SwordDamage
+{
+    class SwordDamage
+    {
+      
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+        public decimal MagicMultiplier = 1.7M;
+        public int Damage;
+        static Random random = new Random();
+
+        public bool isMagic = false;
+        public bool isFlame = false;
+
+        public int Roll3d6()
+        {
+            int roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            return roll;
+        }
+        public int CalculateDamage()
+        {
+
+            int rolled = Roll3d6();
+            Damage = BASE_DAMAGE + rolled;
+
+            if (isMagic)
+            {
+                Damage = (int)((decimal)Damage * MagicMultiplier);
+            }
+
+            if (isFlame)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+
+            Console.WriteLine("\nYou rolled " + rolled + "\nFinal Damage is " + Damage);
+
+            isFlame = false;
+            isMagic = false;
+
+            return Damage;
+        }
+
+
+
+    }
+}
